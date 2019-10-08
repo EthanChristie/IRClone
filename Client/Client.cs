150cbc58bb692e77366fc3dc4fe898735a157f22
Ethan Christie <ethan.j.christie@outlook.com>
2019-10-08 12:57:40 +1000

    Basic messaging for users joining chat

---
+++ Client/Client.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Framing;
+
+namespace Client
+{
+    public class Client
+    {
+        private const string IRCloneServer = "irclone_server";
+        private const string IRCloneClient = "irclone_client";
+
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                Console.WriteLine("Your username: ");
+                var username = Console.ReadLine();
+
+                channel.ExchangeDeclare(IRCloneServer, ExchangeType.Direct);
+                channel.ExchangeDeclare(IRCloneClient, ExchangeType.Fanout);
+                var queueName = channel.QueueDeclare().QueueName;
+
+                var basicProperties = new BasicProperties
+                {
+                    Timestamp = new AmqpTimestamp(DateTime.Now.Ticks)
+                };
+
+                channel.BasicPublish(
+                    exchange: IRCloneServer,
+                    routingKey: "user_joined",
+                    basicProperties: basicProperties,
+                    body: Encoding.UTF8.GetBytes(username)
+                );
+
+
+                channel.QueueBind(
+                    queue: queueName,
+                    exchange: "irclone_client",
+                    routingKey: ""
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var message = Encoding.UTF8.GetString(ea.Body);
+                    Console.WriteLine(message);
+                };
+
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+                Console.ReadLine();
+            }
+                
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Server
+{
+    public class Server
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("irclone_server", ExchangeType.Direct);
+                channel.ExchangeDeclare("irclone_client", ExchangeType.Fanout);
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(
+                    queue: queueName,
+                    exchange: "irclone_server",
+                    routingKey: "user_joined"
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var username = Encoding.UTF8.GetString(ea.Body);
+                    var message = $"<Server>: {username} has joined the chat!";
+
+                    channel.BasicPublish(
+                        exchange: "irclone_client",
+                        routingKey: "user_joined",
+                        body: Encoding.UTF8.GetBytes(message)
+                    );
+
+                    Console.WriteLine("[Debug] " + message);
+                };
+
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+                Console.ReadLine();
+            }
+
+
+
+            Console.WriteLine(" [*] Waiting for messages");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+        }
+    }
